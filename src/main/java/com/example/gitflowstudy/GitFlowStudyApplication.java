a42858b5d59b48a116bd18acae5c0243f771c53b
조성신 <sungsin1030@gmail.com>
2024-07-17 16:51:08 +0900

    Test

--- src/main/java/com/example/gitflowstudy/GitFlowStudyApplication.java
+++ src/main/java/com/example/gitflowstudy/GitFlowStudyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GitFlowStudyApplication {
+public class GitFlowStudyApplication { // Test
 	public static void main(String[] args) {
 		SpringApplication.run(GitFlowStudyApplication.class, args);
 		// Master branch Test Code2
