14acf631d04fe92935e85f44d21cc1614995a92c
DevSung <sungsin1030@gmail.com>
2024-07-17 10:50:56 +0900

    Initial commit

---
+++ src/test/java/com/example/gitflowstudy/GitFlowStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitflowstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFlowStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
